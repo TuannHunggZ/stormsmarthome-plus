75acc350e3fdd9f5d3d21988abf73538a59b33d6
hungvt <hung.vt@zinza.com.vn>
2026-07-22 21:54:46 +0700

    change database schema

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,11 +1,37 @@
-CREATE TABLE historical_load (
-    event_id       BIGINT PRIMARY KEY,
-    timestamp      TIMESTAMP NOT NULL,
-    value          DOUBLE PRECISION NOT NULL,
-    plug_id        INTEGER NOT NULL,
+CREATE TABLE plug_average (
+    window_size    INTEGER NOT NULL,
+    slice_index    BIGINT NOT NULL,
+
+    house_id       INTEGER NOT NULL,
     household_id   INTEGER NOT NULL,
-    house_id       INTEGER NOT NULL
+    plug_id        INTEGER NOT NULL,
+
+    average_load   DOUBLE PRECISION NOT NULL,
+
+    PRIMARY KEY (
+        window_size,
+        slice_index,
+        house_id,
+        household_id,
+        plug_id
+    )
 );
 
-CREATE INDEX idx_timestamp
-ON historical_load(timestamp);
+CREATE INDEX idx_slice_index ON plug_average(slice_index);
+
+CREATE TABLE house_average (
+    window_size    INTEGER NOT NULL,
+    slice_index    BIGINT NOT NULL,
+
+    house_id       INTEGER NOT NULL,
+
+    average_load   DOUBLE PRECISION NOT NULL,
+
+    PRIMARY KEY (
+        window_size,
+        slice_index,
+        house_id
+    )
+);
+
+CREATE INDEX idx_house_slice_index ON house_average(slice_index);
